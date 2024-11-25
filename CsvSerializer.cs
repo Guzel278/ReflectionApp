43d0ff3a63b60111863b94a2f23e8257d83cde4f
Guzel278 <71883716+Guzel278@users.noreply.github.com>
2024-11-25 21:20:07 +0300

    Initial commit

---
+++ CsvSerializer.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Reflection;
+
+public class CsvSerializer
+{
+    // Сериализация объекта в строку CSV
+    public string Serialize<T>(T obj)
+    {
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var values = properties.Select(p => p.GetValue(obj)?.ToString() ?? string.Empty);
+        return string.Join(",", values);
+    }
+
+    // Десериализация строки CSV в объект
+    public T Deserialize<T>(string csv) where T : new()
+    {
+        var obj = new T();
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var values = csv.Split(',');
+
+        for (int i = 0; i < properties.Length && i < values.Length; i++)
+        {
+            var property = properties[i];
+            if (property.CanWrite)
+            {
+                var value = Convert.ChangeType(values[i], property.PropertyType);
+                property.SetValue(obj, value);
+            }
+        }
+        return obj;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Text.Json;
+
+
+var serializer = new CsvSerializer();
+var testObject = F.Get();
+const int iterations = 100000;
+
+// Замер времени на кастомную сериализацию
+var stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < iterations; i++)
+{
+    var csv = serializer.Serialize(testObject);
+}
+stopwatch.Stop();
+Console.WriteLine($"Custom serialization time: {stopwatch.ElapsedMilliseconds} ms");
+
+// Замер времени на стандартную JSON сериализацию
+stopwatch.Restart();
+for (int i = 0; i < iterations; i++)
+{
+    var json = JsonSerializer.Serialize(testObject);
+}
+stopwatch.Stop();
+Console.WriteLine($"JSON serialization time: {stopwatch.ElapsedMilliseconds} ms");
+
+// Замер времени вывода в консоль
+var csvString = serializer.Serialize(testObject);
+stopwatch.Restart();
+Console.WriteLine(csvString);
+stopwatch.Stop();
+Console.WriteLine($"Console output time: {stopwatch.ElapsedMilliseconds} ms");
+
+// Замер времени на десериализацию
+stopwatch.Restart();
+for (int i = 0; i < iterations; i++)
+{
+    var deserializedObject = serializer.Deserialize<F>(csvString);
+}
+stopwatch.Stop();
+Console.WriteLine($"Custom deserialization time: {stopwatch.ElapsedMilliseconds} ms");
+
+// Стандартная JSON десериализация
+var jsonString = JsonSerializer.Serialize(testObject);
+stopwatch.Restart();
+for (int i = 0; i < iterations; i++)
+{
+    var deserializedObject = JsonSerializer.Deserialize<F>(jsonString);
+}
+stopwatch.Stop();
+Console.WriteLine($"JSON deserialization time: {stopwatch.ElapsedMilliseconds} ms");
+
